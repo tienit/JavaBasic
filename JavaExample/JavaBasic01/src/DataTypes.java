e994244a54e4dd9f7a4f49e9b67a5bf256221a88
Tien Nguyen Quyet <bktien@gmail.com>
2019-06-19 23:10:08 +0700

    - Update document to "Java Strings" chapter - Including Examples

---
+++ JavaExample/JavaBasic01/src/DataTypes.java
@@ -0,0 +1,48 @@
+
+public class DataTypes {
+
+//	public static void main(String[] args) {
+//		
+//		byte myNum = 100; // -128 to 127
+//		System.out.println(myNum);
+//
+//		short myNum1 = 5000;
+//		System.out.println(myNum1);
+//
+//		int myNum2 = 100000;
+//		System.out.println(myNum2);
+//		
+//		long myNum3 = 15000000000L;
+//		System.out.println(myNum3);
+//
+//		float myNum4 = 19.99f;
+//		System.out.println(myNum4);
+//
+//		double myNum5 = 19.99d;
+//		System.out.println(myNum5);
+//
+//		float f1 = 35e3f;
+//		double d1 = 12E4d;
+//		System.out.println(f1);
+//		System.out.println(d1);
+//
+//		boolean isJavaFun = true;
+//		boolean isFishTasty = false;
+//		System.out.println(isJavaFun);    // Outputs true
+//		System.out.println(isFishTasty);  // Outputs false
+//		
+//		char myGrade = 'B';
+//		System.out.println(myGrade);
+//
+//		char x = 97, y = 98, z = 99;
+//		System.out.println(x); 
+//		System.out.println(y);
+//		System.out.println(z);
+//		
+//		String greeting = "Hello World";
+//		System.out.println(greeting);
+//
+//
+//	}
+
+}
---
+++ JavaExample/JavaBasic01/src/TypeCasting.java
@@ -0,0 +1,24 @@
+
+public class TypeCasting {
+
+//	public static void main(String[] args) {
+//		
+//		// Widening Casting: automatic
+//		
+//		int myNum = 120;
+//		
+//		double myDouble = myNum; // Widening Casting: int -> double
+//		
+//		System.out.println(myDouble);
+//		
+//		// Narrowing Casting: manual
+//		
+//		double myD = 9.78d;
+//		
+//		int myI = (int) myD; // Narrowing casting: double -> int
+//		
+//		System.out.println(myD);
+//		System.out.println(myI);
+//	}
+
+}
