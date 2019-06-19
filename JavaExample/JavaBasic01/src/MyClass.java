e5f2539aacc66462247875e1698ba9c286eff827
Tien Nguyen Quyet <bktien@gmail.com>
2019-06-19 21:45:52 +0700

    Example Project for Java Learning - JavaBasic 01

---
+++ JavaExample/JavaBasic01/src/MyClass.java
@@ -0,0 +1,25 @@
+
+public class MyClass {
+
+//	public static void main(String[] args) {
+//		
+//		int myNum = 5;
+//		float myFloatNum = 5.99f;
+//		char myLetter = 'D';
+//		boolean myBool = true; 
+//		String myText = "Hello";
+//
+//		
+//		String firstName = "John ";
+//		String lastName = "Doe";
+//		String fullName = firstName + lastName;
+//		System.out.println(fullName);
+//
+//		int x = 5;
+//		int y = 6;
+//		System.out.println(x + y);
+//		
+//		System.out.println("Hello World " + myText);
+//	}
+
+}
