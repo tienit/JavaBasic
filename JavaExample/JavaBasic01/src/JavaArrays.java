57620f464310e4f3dcec176f713baf15617d11a7
truongcavoi <truonghd0712@gmail.com>
2019-06-24 22:01:44 +0700

    update

---
+++ JavaExample/JavaBasic01/src/JavaArrays.java
@@ -0,0 +1,17 @@
+public class JavaArrays {
+	public static void main(String[] args)
+	{
+	//int i =0;
+	String[] arrGiaDinh = new String[5];
+			arrGiaDinh[0] = "Bố";
+			arrGiaDinh[1] = "Mẹ";
+			arrGiaDinh[2] = "Anh";
+			arrGiaDinh[3] = "Chị";
+			arrGiaDinh[4] = "Cháu trai";
+			//arrGiaDinh[5] = "Cháu gái";
+	for (int i=0; i<arrGiaDinh.length-1; i++)
+	{
+		System.out.println(arrGiaDinh[i]);
+	}
+	}
+}
